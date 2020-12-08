29ad1b73f7cd84794b305e202fa6c685de329b52
kergene <keremergene@gmail.com>
2020-12-08 11:23:40 +0000

    Day 8 solutions

---
+++ code_day_8.py
@@ -0,0 +1,56 @@
+def get_data():
+    with open("input_day_8.txt") as f:
+        data = f.read().replace('+','').splitlines()
+    data = [instruction.split() for instruction in data]
+    return data
+
+def run_script(instructions):
+    accumulator, row, seens = 0, 0, set()
+    while row not in seens:
+        seens.add(row)
+        if instructions[row][0] == 'acc':
+            accumulator += int(instructions[row][1])
+            row += 1
+        elif instructions[row][0] == 'jmp':
+            row += int(instructions[row][1])
+        elif instructions[row][0] == 'nop':
+            row += 1
+        else:
+            raise ValueError()
+    return accumulator
+
+def test_fix(instructions):
+    accumulator, row, seens = 0, 0, set()
+    n = len(instructions)
+    while row not in seens:
+        if row == n:
+            return accumulator
+        seens.add(row)
+        if instructions[row][0] == 'acc':
+            accumulator += int(instructions[row][1])
+            row += 1
+        elif instructions[row][0] == 'jmp':
+            row += int(instructions[row][1])
+        elif instructions[row][0] == 'nop':
+            row += 1
+        else:
+            raise Error()
+    return 'Failed'
+
+def de_corrupt(instructions):
+    for row in range(len(instructions)):
+        if instructions[row][0] == 'jmp':
+            instructions[row][0] = 'nop'
+            acc = test_fix(instructions)
+            instructions[row][0] = 'jmp'
+        elif instructions[row][0] == 'nop':
+            instructions[row][0] = 'jmp'
+            acc = test_fix(instructions)
+            instructions[row][0] = 'nop'
+        if acc != 'Failed':
+            return acc
+
+if __name__ == "__main__":
+    instructions = get_data()
+    print(run_script(instructions))
+    print(de_corrupt(instructions))
