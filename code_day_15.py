e0896b8d944e791f54b2e6bf06475f1574d51805
Kerem Ergene <kerem@Kerems-MacBook-Pro.local>
2020-12-15 05:40:12 +0000

    Day 15 solutions

---
+++ code_day_15.py
@@ -0,0 +1,32 @@
+def get_data(day):
+    if day < 10:
+        day = '0'+str(day)
+    with open(f"input_day_{day}.txt") as f:
+        data = f.read().split(',')
+    data = [int(datum) for datum in data]
+    return data
+
+def find_nth_term(data, n):
+    index_lookup = {}
+    for idx, val in enumerate(data[:-1]):
+        index_lookup[val] = idx
+    last = data[-1]
+    for i in range(len(data), n):
+        if last in index_lookup:
+            next_term = i - index_lookup[last] - 1
+            index_lookup[last] = i - 1
+            last = next_term
+        else:
+            next_term = 0
+            index_lookup[last] = i - 1
+            last = next_term
+    return last
+
+def main():
+    day = 15
+    data = get_data(day)
+    print(find_nth_term(data, 2020))
+    print(find_nth_term(data, 30000000))
+
+if __name__ == "__main__":
+    main()
