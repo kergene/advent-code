0dc4a3f33c6be8c1303e40feb8b167c3c5368729
Kerem Ergene <kerem@Kerems-MacBook-Pro.local>
2020-12-19 18:17:30 +0000

    Major performance improvement

--- code_day_19.py
+++ code_day_19.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+from itertools import product
+
 def get_data(day):
     if day < 10:
         day = '0'+str(day)
@@ -5,7 +8,8 @@ def get_data(day):
         data = f.read().split('\n\n')
     rules = dict(preprocess(datum) for datum in data[0].splitlines())
     messages = data[1].splitlines()
-    return rules, messages
+    inv_t_p, inv_nt_p, n_nt = cnf(rules)
+    return inv_t_p, inv_nt_p, n_nt, messages
 
 def preprocess(datum):
     datum = datum.split(':')
@@ -37,76 +41,78 @@ def cnf(rules):
                     else:
                         new_outputs.append([choice[0]])
                 rules[base] = new_outputs
-    nonterminal_rules = dict()
-    terminal_rules = dict()
-    max_idx = 0
+    nt_prods = dict()
+    t_prods = dict()
+    n_nt = 0
     for rule_idx, output in rules.items():
-        if rule_idx > max_idx:
-            max_idx = rule_idx
+        if rule_idx > n_nt:
+            n_nt = rule_idx
         if len(output[0]) == 1:
-            terminal_rules[rule_idx] = output
+            t_prods[rule_idx] = output
         else:
-            nonterminal_rules[rule_idx] = output
-    return terminal_rules, nonterminal_rules, max_idx
+            nt_prods[rule_idx] = output
+    inv_t_p, inv_nt_p = invert_rules(t_prods, nt_prods)
+    return inv_t_p, inv_nt_p, n_nt
 
-def cyk(string, terminal_rules, nonterminal_rules, r):
+def invert_rules(t_prods, nt_prods):
+    # inverts rule dictionaries for performance
+    inv_t_p = defaultdict(set)
+    for nonterminal, outputs in t_prods.items():
+        for production in outputs:
+            inv_t_p[production[0]].add(nonterminal)
+    inv_nt = defaultdict(set)
+    for nonterminal, outputs in nt_prods.items():
+        for production in outputs:
+            inv_nt[tuple(production)].add(nonterminal)
+    return inv_t_p, inv_nt
+
+def cyk(string, inv_t, inv_nt):
     # runs CYK algorithm
     n = len(string)
     M = [[set() for _ in range(n)] for _ in range(n)]
     for i in range(len(string)):
-        for T, terminal_choices in terminal_rules.items():
-            for terminal in terminal_choices:
-                if string[i] == terminal[0]:
-                    M[i][i].add(T)
+        for nonterminal in inv_t[string[i]]:
+            M[i][i].add(nonterminal)
     for length in range(1, n):
         for start in range(0, n - length):
             for partition in range(0, length):
                 L = M[start][start + partition]
                 R = M[start + partition + 1][start + length]
-                for A, nonterminal_choices in nonterminal_rules.items():
-                    for B, C in nonterminal_choices:
-                        if B in L and C in R:
-                            M[start][start + length].add(A)
+                for nt_prod in product(L, R):
+                    for A in inv_nt[nt_prod]:
+                        M[start][start + length].add(A)
     return 0 in M[0][-1]
 
-def easy_cnf(rules, messages):
-    terminal_rules, nonterminal_rules, max_idx = cnf(rules)
+def easy_cnf(inv_t_p, inv_nt_p, messages):
     tot = 0
     for line in messages:
-        tot += cyk(line, terminal_rules, nonterminal_rules, max_idx)
+        tot += cyk(line, inv_t_p, inv_nt_p)
     return tot
 
-def add_nt_rules(nonterminal_rules, max_idx, extra_nt_rules):
-    # adds slightly more complex rules to CNF
-    for nonterminal, outputs in extra_nt_rules.items():
-        for output in outputs:
-            if len(output) == 2:
-                nonterminal_rules[nonterminal].append(output)
-            else:
-                while len(output) > 2:
-                    nonterminal_rules[max_idx + 1] = [output[-2:]]
-                    output = output[:-1]
-                    output[-1] = max_idx + 1
-                    max_idx += 1
-                nonterminal_rules[nonterminal].append(output)
-    return nonterminal_rules, max_idx
+def add_nt_rules(inv_nt_p, n_nt, extra_nt_p):
+    # adds slightly more complex rules to inverted cnf
+    for nonterminal, outputs in extra_nt_p.items():
+        for production in outputs:
+            while len(production) > 2:
+                n_nt += 1
+                inv_nt_p[tuple(production[-2:])].add(n_nt)
+                production = production[:-1]
+                production[-1] = n_nt
+            inv_nt_p[tuple(production)].add(nonterminal)
+    return inv_nt_p
 
-def harder_cnf(rules, messages):
-    extra_nt_rules = dict()
-    extra_nt_rules[8] = [[42, 8]]
-    extra_nt_rules[11] = [[42, 11, 31]]
-    terminal_rules, nonterminal_rules, max_idx = cnf(rules)
-    nonterminal_rules, max_idx = add_nt_rules(nonterminal_rules, max_idx, extra_nt_rules)
-    tot = 0
-    for line in messages:
-        tot += cyk(line, terminal_rules, nonterminal_rules, max_idx)
-    return tot
+def harder_cnf(inv_t_p, inv_nt_p, n_nt, messages):
+    extra_nt_p = dict()
+    extra_nt_p[8] = [[42, 8]]
+    extra_nt_p[11] = [[42, 11, 31]]
+    inv_nt_p = add_nt_rules(inv_nt_p, n_nt, extra_nt_p)
+    return easy_cnf(inv_t_p, inv_nt_p, messages)
 
 def main():
     day = 19
-    rules, messages = get_data(day)
-    print(easy_cnf(rules, messages))
-    print(harder_cnf(rules, messages))
+    inv_t_p, inv_nt_p, n_nt, messages = get_data(day)
+    print(easy_cnf(inv_t_p, inv_nt_p, messages))
+    print(harder_cnf(inv_t_p, inv_nt_p, n_nt, messages))
 
 if __name__ == "__main__":
     main()
