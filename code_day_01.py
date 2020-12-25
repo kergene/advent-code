5d704fd587f8345a2a98fb42af8549d3ea6ef6cd
Kerem Ergene <kerem@Kerems-MacBook-Pro.local>
2020-12-25 11:37:26 +0000

    Significant performance improvemnt

--- code_day_01.py
+++ code_day_01.py
@@ -1,6 +1,7 @@
 from itertools import combinations
 from math import prod
 
+
 def get_data(day):
     if day < 10:
         day = '0'+str(day)
@@ -9,15 +10,33 @@ def get_data(day):
     data = list(int(i) for i in data)
     return data
 
-def find_sum(data, n):
-    for i in combinations(data, n):
-        if sum(i) == 2020: return prod(i)
+
+def find_n_sum(data, n, target=2020):
+    if n > 2:
+        for idx in range(len(data) - 1):
+            value = data[idx]
+            product, found = find_n_sum(data[idx+1:], n - 1, target - value)
+            if found:
+                return value * product, True
+        else:
+            return 0, False
+    else:
+        minus = set()
+        for value in data:
+            if value in minus:
+                return value * (target - value), True
+            else:
+                minus.add(target - value)
+        else:
+            return 0, False
+
 
 def main():
     day = 1
     data = get_data(day)
-    print(find_sum(data, 2))
-    print(find_sum(data, 3))
+    print(find_n_sum(data, 2)[0])
+    print(find_n_sum(data, 3)[0])
+
 
 if __name__ == "__main__":
     main()
